c62c2f79796d779e82ece14aa1f344e8518226d9
Your Name <mohamaduhadijatu@gmail.com>
2025-06-13 13:51:19 -0400

    product card price updated with pipe

--- src/app/components/productcard/productcard.component.ts
+++ src/app/components/productcard/productcard.component.ts
@@ -1,10 +1,11 @@
 import { Component,Input } from '@angular/core';
 import { AddToCartComponent } from '../add-to-cart/add-to-cart.component';
 import { Dessert } from '../../../../public/datainterface';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-productcard',
-  imports: [AddToCartComponent],
+  imports: [AddToCartComponent,CurrencyPipe],
   templateUrl: './productcard.component.html',
   styleUrl: './productcard.component.scss'
 })
